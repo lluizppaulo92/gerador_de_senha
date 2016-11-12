4b9400e22343e1281c39e77b867d756d1a3b739c
Luiz Paulo <luiz_1.8vs@hotmail.com>
2016-11-12 17:41:40 -0200

    retirei o teste

--- App1/MainPage.xaml.cs
+++ App1/MainPage.xaml.cs
@@ -7,8 +7,7 @@ using System.Text.RegularExpressions;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 // Colaboradores: Luiz Paulo, Matheus Jose, Carlos Eduardo
-// mais uma modificação
-// teste
+
 
 
 namespace App1
